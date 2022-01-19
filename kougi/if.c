d13b87e5ecececa425c25a374d3dfa679a088c37
root <root@DESKTOP-MP59QNU.localdomain>
2022-01-19 22:52:04 +0900

    Update

---
+++ kougi/if.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void){
+  int a, b;
+
+  scanf("%d", &a);
+  scanf("%d", &b);
+
+  if(a < b)
+    printf("bの方が大きい");
+      else if(a > b)
+        printf("aの方が大きい");
+
+
+}
